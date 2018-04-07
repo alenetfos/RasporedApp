280fef9c3b7fe2c7e2286dd354fe8c02f02fc634
alen_etfos <alenhalilovi490@gmail.com>
2018-04-07 12:14:34 +0200

    ..
    Database file

--- raspored.sql
+++ raspored.sql
@@ -1,3 +1,9 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: raspored
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.1.28-MariaDB
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -17,12 +23,12 @@ DROP TABLE IF EXISTS `godina`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `godina` (
-  `id_godina` int(11) NOT NULL,
-  `broj_godine` varchar(255) DEFAULT NULL,
-  `smjer_id_smjer` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_godina`),
-  KEY `FKrb4wdxuhovou7xm0e61subkfn` (`smjer_id_smjer`),
-  CONSTRAINT `FKrb4wdxuhovou7xm0e61subkfn` FOREIGN KEY (`smjer_id_smjer`) REFERENCES `smjer` (`id_smjer`)
+  `idGodina` int(11) NOT NULL,
+  `brojGodine` varchar(255) DEFAULT NULL,
+  `smjer_idSmjer` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idGodina`),
+  KEY `FK7aba434je8ugxs4p53ley3n8o` (`smjer_idSmjer`),
+  CONSTRAINT `FK7aba434je8ugxs4p53ley3n8o` FOREIGN KEY (`smjer_idSmjer`) REFERENCES `smjer` (`idSmjer`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -32,7 +38,7 @@ CREATE TABLE `godina` (
 
 LOCK TABLES `godina` WRITE;
 /*!40000 ALTER TABLE `godina` DISABLE KEYS */;
-INSERT INTO `godina` VALUES (3,'1.godina',NULL);
+INSERT INTO `godina` VALUES (3,'1.Godina',1),(4,'2.Godina',2);
 /*!40000 ALTER TABLE `godina` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -54,7 +60,7 @@ CREATE TABLE `hibernate_sequence` (
 
 LOCK TABLES `hibernate_sequence` WRITE;
 /*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
-INSERT INTO `hibernate_sequence` VALUES (5),(5),(5);
+INSERT INTO `hibernate_sequence` VALUES (7),(7),(7),(7),(7);
 /*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -66,13 +72,13 @@ DROP TABLE IF EXISTS `predmet`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `predmet` (
-  `id_predmet` int(11) NOT NULL,
-  `broj_sati` decimal(19,2) DEFAULT NULL,
+  `idPredmet` int(11) NOT NULL,
+  `brojSati` int(11) NOT NULL,
   `naziv` varchar(255) DEFAULT NULL,
-  `godina_id_godina` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_predmet`),
-  KEY `FK9i6it9hqkpa7nga5ucpss59xr` (`godina_id_godina`),
-  CONSTRAINT `FK9i6it9hqkpa7nga5ucpss59xr` FOREIGN KEY (`godina_id_godina`) REFERENCES `godina` (`id_godina`)
+  `godina_idGodina` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idPredmet`),
+  KEY `FKmnb9s6tlc9cfo5n90htdf2hyl` (`godina_idGodina`),
+  CONSTRAINT `FKmnb9s6tlc9cfo5n90htdf2hyl` FOREIGN KEY (`godina_idGodina`) REFERENCES `godina` (`idGodina`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -82,8 +88,85 @@ CREATE TABLE `predmet` (
 
 LOCK TABLES `predmet` WRITE;
 /*!40000 ALTER TABLE `predmet` DISABLE KEYS */;
-INSERT INTO `predmet` VALUES (1,70.00,'Matematika',NULL);
+INSERT INTO `predmet` VALUES (5,60,'Matematika',3),(6,60,'Fizika',4);
 /*!40000 ALTER TABLE `predmet` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `predmet_profesor`
+--
+
+DROP TABLE IF EXISTS `predmet_profesor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `predmet_profesor` (
+  `predmeti_idPredmet` int(11) NOT NULL,
+  `profesori_idProfesor` int(11) NOT NULL,
+  KEY `FK2e7101feavywh066bfo1jsb26` (`profesori_idProfesor`),
+  KEY `FKl8l1hsu7g3fqvf0ajolp4v4gj` (`predmeti_idPredmet`),
+  CONSTRAINT `FK2e7101feavywh066bfo1jsb26` FOREIGN KEY (`profesori_idProfesor`) REFERENCES `profesor` (`idProfesor`),
+  CONSTRAINT `FKl8l1hsu7g3fqvf0ajolp4v4gj` FOREIGN KEY (`predmeti_idPredmet`) REFERENCES `predmet` (`idPredmet`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `predmet_profesor`
+--
+
+LOCK TABLES `predmet_profesor` WRITE;
+/*!40000 ALTER TABLE `predmet_profesor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `predmet_profesor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `predmet_student`
+--
+
+DROP TABLE IF EXISTS `predmet_student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `predmet_student` (
+  `predmeti_idPredmet` int(11) NOT NULL,
+  `studenti_idStudent` int(11) NOT NULL,
+  KEY `FKeqhbipcsybipq3hkeetd0ddk8` (`studenti_idStudent`),
+  KEY `FKejeceua5tpsxoysnekaueaeyv` (`predmeti_idPredmet`),
+  CONSTRAINT `FKejeceua5tpsxoysnekaueaeyv` FOREIGN KEY (`predmeti_idPredmet`) REFERENCES `predmet` (`idPredmet`),
+  CONSTRAINT `FKeqhbipcsybipq3hkeetd0ddk8` FOREIGN KEY (`studenti_idStudent`) REFERENCES `student` (`idStudent`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `predmet_student`
+--
+
+LOCK TABLES `predmet_student` WRITE;
+/*!40000 ALTER TABLE `predmet_student` DISABLE KEYS */;
+/*!40000 ALTER TABLE `predmet_student` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `profesor`
+--
+
+DROP TABLE IF EXISTS `profesor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `profesor` (
+  `idProfesor` int(11) NOT NULL,
+  `ime` varchar(255) DEFAULT NULL,
+  `oib` int(11) NOT NULL,
+  `prezime` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`idProfesor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `profesor`
+--
+
+LOCK TABLES `profesor` WRITE;
+/*!40000 ALTER TABLE `profesor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `profesor` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -94,10 +177,10 @@ DROP TABLE IF EXISTS `smjer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `smjer` (
-  `id_smjer` int(11) NOT NULL,
-  `naziv` longtext,
-  `opis` longtext,
-  PRIMARY KEY (`id_smjer`)
+  `idSmjer` int(11) NOT NULL,
+  `naziv` varchar(255) DEFAULT NULL,
+  `opis` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`idSmjer`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -107,8 +190,35 @@ CREATE TABLE `smjer` (
 
 LOCK TABLES `smjer` WRITE;
 /*!40000 ALTER TABLE `smjer` DISABLE KEYS */;
-INSERT INTO `smjer` VALUES (4,'Računarstvo','Preddiplomski Studij');
+INSERT INTO `smjer` VALUES (1,'Informatika','Stručni Studij'),(2,'Računarstvo','Preddiplomski Studij');
 /*!40000 ALTER TABLE `smjer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `student` (
+  `idStudent` int(11) NOT NULL,
+  `brojDolaznosti` int(11) NOT NULL,
+  `brojIndexa` varchar(255) DEFAULT NULL,
+  `ime` varchar(255) DEFAULT NULL,
+  `postotak` decimal(19,2) DEFAULT NULL,
+  `prezime` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`idStudent`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -120,4 +230,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-03-28 11:42:07
+-- Dump completed on 2018-04-07 12:13:45
